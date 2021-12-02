09e6192666524165e7f4b52b73b961d96b4b9f95
kiranmmd <kiranmakemydigital@gmail.com>
2021-12-02 11:31:20 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Memo from "./components/useMemo/Memo";
+
+function App() {
+  return (
+    <div className="">
+      <Memo />
+    </div>
+  );
+}
+
+export default App;
